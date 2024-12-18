bdea1fb61718409740694dfd14a1e1384ac04b4a
Rahulravib <rahul.ravi@bootlabstech.com>
2024-12-18 18:20:58 +0530

    user accout management using slack

--- user_account_automation.sh
+++ user_account_automation.sh
@@ -4,12 +4,31 @@
 #For user to change password on the first login.
 
 #!/bin/bash
-read -p "Enter the Username: " USERNAME
-echo $USERNAME
-Existing_User=$(cat /etc/passwd | grep -i -w $USERNAME | cut -d ":" -f 1)
-echo $Existing_User
-if [ "${USERNAME}" = "${Existing_User}" ]; then
-    echo "The User with Username ${USERNAME} already exists.Try a differnt Username"
+# read -p "Enter the Username: " USERNAME
+# echo $USERNAME
+SLACK_WEB="https://hooks.slack.com/services/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"
+if [ $# -gt 0 ]; then
+    USERNAME=$1
+    Existing_User=$(cat /etc/passwd | grep -i -w $USERNAME | cut -d ":" -f 1)
+    # echo $Existing_User
+    if [ "${USERNAME}" = "${Existing_User}" ]; then
+        echo "The User with Username ${USERNAME} already exists.Try a differnt Username"
+    else
+        echo "Lets create the user with Username ${USERNAME}"
+        useradd -m $USERNAME
+        echo "User with Username ${USERNAME} has been created"
+        user=$(cat /etc/passwd | grep -i $USERNAME | awk '{print $1}' | cut -d ":" -f 1)
+        echo "${user}"
+        echo "please provide the password for ${USERNAME}"
+        SPEC=$(echo '$#@^&*()_' | fold -1 | shuf | head -1)
+        PASSWORD="India@${RANDOM}${SPEC}"
+        echo "${USERNAME}:${PASSWORD}" | chpasswd
+        # echo "${PASSWORD}"
+        passwd -e ${USERNAME}
+        # echo "Please check Creds with Username ${USERNAME} and Password ${PASSWORD}"
+        curl -X POST ${SLACK_WEB} -sL -H 'Content-type: application/json' --data "{"text": \"Username is: ${USERNAME}\"}" >>/dev/null
+        curl -X POST ${SLACK_WEB} -sL -H 'Content-type: application/json' --data "{"text": \"Temporary Password Is: ${PASSWORD}  Reset This Password Immediatly.\"}" >>/dev/null
+    fi
 else
-    echo "Lets create the user with Username ${USERNAME}"
+    echo "you have given $# arguments.Please atleast provide one username"
 fi
